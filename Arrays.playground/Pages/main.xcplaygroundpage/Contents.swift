fc75c658b1c9d7ab2823a21ae982a60ac51cac79
4Ring <ukrainianslavic@gmail.com>
2017-01-05 00:11:25 -0800

    Completed Lab

--- Arrays.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ Arrays.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,7 +14,7 @@
 // write your code here
 
 
-
+var list = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 
@@ -23,14 +23,16 @@
  */
 // write your code here
 
-
+var shoppingList: [String] = list
 
 
 
 /*: question3
  ### 3. Initialize an array called 'futureShoppingList' **with** a type specified and then add the string values "Bread", "Butter", "Cheese", "Lettuce", "Tomatoes".
  */
-// write your code here
+var futureShoppingList: [String]
+
+futureShoppingList = list
 
 
 
@@ -43,7 +45,7 @@
 // write your code here
 
 
-
+let cheeseSandwich: [String] = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 
@@ -53,7 +55,7 @@
 // write your code here
 
 
-
+var dessertList: [String] = ["cookie dough", "icecream"]
 
 
 
@@ -64,7 +66,7 @@
 // write your code here
 
 
-
+var afternoonAttendance: [Int] = [2, 10, 3, 15, 7]
 
 
 
@@ -75,8 +77,9 @@
  */
 // write your code here
 
+var itemToPrint = shoppingList[1]
 
-
+print(itemToPrint)
 
 
 
@@ -88,7 +91,9 @@
 // write your code here
 
 
+futureShoppingList[2] = "Chicken"
 
+print(futureShoppingList)
 
 
 
@@ -100,15 +105,22 @@
 // write your code here
 
 
+func lifesEssential(ingredients: [String]) -> Bool {
+    if ingredients[0] == "Bread" {
+        return true
+    } else {
+        return false
+    }
+}
 
-
-
+lifesEssential(ingredients: ["Onion", "Cheese", "Lettuce"])
 
 /*: question10
  ### 10. Pass the 'shoppingList' array to the 'lifesEssential' function and print the result.
  */
 // write your code here
 
+print(lifesEssential(ingredients: shoppingList))
 
 
 
@@ -120,6 +132,8 @@
  ### 11. Pass the 'desertList' array to the 'lifesEssential' function and print the result.
  */
 // write your code here
+
+print(lifesEssential(ingredients: dessertList))
 
 
 
