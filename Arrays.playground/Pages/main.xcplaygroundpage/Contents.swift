6df282e1aa685bb267b99f41b52e1dea8787a2a5
JimCampagno <jimcampagno@gmail.com>
2017-01-23 13:50:34 -0500

    Update playground file

---
+++ Arrays.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -0,0 +1,125 @@
+/*: Outline
+ 
+ 
+ # Arrays
+ 
+ ## Readings associated with this lab
+ 
+ * [Arrays](https://github.com/learn-co-curriculum/swift-array-readme)
+ 
+ */
+/*: question1
+ ### 1. Write an array called 'list' **without** a type specified that contains the string values "Bread", "Butter", "Cheese", "Lettuce", "Tomatoes".
+ */
+    // write your code here
+
+
+
+
+
+
+/*: question2
+ ### 2. Write an array called 'shoppingList' **with** a type specified that contains the string values "Bread", "Butter", "Cheese", "Lettuce", "Tomatoes".
+ */
+// write your code here
+
+
+
+
+
+/*: question3
+ ### 3. Initialize an array called 'futureShoppingList' **with** a type specified and then add the string values "Bread", "Butter", "Cheese", "Lettuce", "Tomatoes".
+ */
+// write your code here
+
+
+
+
+
+
+/*: question4
+ ### 4. Write an array called 'cheeseSandwich' **with** a type specified whose value will never change (constant) and contains the string values "Bread", "Butter", "Cheese", "Lettuce", "Tomatoes".
+ */
+// write your code here
+
+
+
+
+
+
+/*: question5
+ ### 5. Create a second array called 'dessertList' with a type specified which contains the string values "Cookie dough", "Icecream"
+ */
+// write your code here
+
+
+
+
+
+
+/*: question6
+ ### 6. Write an array called 'afternoonAttendance' **with** a type specified that contains the integer values 2, 10, 3, 15, 7.
+ */
+// write your code here
+
+
+
+
+
+
+
+
+/*: question7
+ ### 7. Assign the value at the second position in the 'shoppingList' array to a variable called 'itemToPrint' and print it to the screen.
+ */
+// write your code here
+
+
+
+
+
+
+
+
+/*: question8
+ ### 8. Change the value of 'Cheese' in the 'futureShoppingList' array to 'Chicken' and then print the array to the console.
+ */
+// write your code here
+
+
+
+
+
+
+
+
+/*: question9
+ ### 9. Create a function named 'lifesEssential' that accepts an array of strings as a parameter called 'ingredients' and returns a boolean. Inside the function, check to see if the array passed to the function contains the value "Bread" at position 0. If it does, return a true value, if not false.
+ */
+// write your code here
+
+
+
+
+
+
+/*: question10
+ ### 10. Pass the 'shoppingList' array to the 'lifesEssential' function and print the result (the result being the value which is returned as a result to the call to the lifesEssential function).
+ */
+// write your code here
+
+
+
+
+
+
+
+
+/*: question11
+ ### 11. Pass the 'desertList' array to the 'lifesEssential' function and print the result.
+ */
+// write your code here
+
+
+
+
