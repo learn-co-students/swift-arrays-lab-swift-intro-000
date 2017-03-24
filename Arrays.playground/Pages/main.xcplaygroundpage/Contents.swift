2cd216b2077d6ae1a6ca5e6425619822c26599ff
John Doe <dssp@freenet.de>
2017-03-24 18:16:08 +0100

    Completed Array's Lab

--- Arrays.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ Arrays.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -12,9 +12,10 @@
  ### 1. Write an array called 'list' **without** a type specified that contains the string values "Bread", "Butter", "Cheese", "Lettuce", "Tomatoes".
  */
     // write your code here
+let list = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
-
-
+// Answer 1
+var list2 = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 
@@ -22,17 +23,21 @@
  ### 2. Write an array called 'shoppingList' **with** a type specified that contains the string values "Bread", "Butter", "Cheese", "Lettuce", "Tomatoes".
  */
 // write your code here
+let shoppingList : [String] = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
-
-
+// 2
+var shoppingList2: [String] = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 /*: question3
  ### 3. Initialize an array called 'futureShoppingList' **with** a type specified and then add the string values "Bread", "Butter", "Cheese", "Lettuce", "Tomatoes".
  */
 // write your code here
+var futureShoppingList : [String] = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
-
+// Answer 3
+var futureShoppingList2: [String]
+futureShoppingList2 = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 
@@ -42,9 +47,10 @@
  */
 // write your code here
 
+let cheeseSandwich: [String] = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
-
-
+// Answer 4
+let cheeseSandwich2: [String] = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 /*: question5
@@ -52,8 +58,10 @@
  */
 // write your code here
 
+let desertList: [String] =  ["Cookie dough", "Icecream"]
 
-
+// Answer 5
+var desertList2: Array<String> = ["Cookie dough", "Icecream"]
 
 
 
@@ -61,8 +69,10 @@
  ### 6. Write an array called 'afternoonAttendance' **with** a type specified that contains the integer values 2, 10, 3, 15, 7.
  */
 // write your code here
+let afternoonAttendance : [Int] = [2, 10, 3, 15, 7]
 
-
+// Answer 6
+var afternoonAttendance2: [Int] = [2, 10, 3, 15, 7]
 
 
 
@@ -73,9 +83,12 @@
  ### 7. Assign the value at the second position in the 'shoppingList' array to a variable called 'itemToPrint' and print it to the screen.
  */
 // write your code here
+var itemToPrint = shoppingList[1]
+print(itemToPrint)
 
-
-
+// Answer 7
+let itemToPrint2 = shoppingList[1]
+print(itemToPrint)
 
 
 
@@ -85,12 +98,13 @@
  ### 8. Change the value of 'Cheese' in the 'futureShoppingList' array to 'Chicken' and then print the array to the console.
  */
 // write your code here
+futureShoppingList[2] = "Chicken"
+print(futureShoppingList)
 
 
-
-
-
-
+// Anwser 8
+futureShoppingList[2] = "Chicken"
+print(futureShoppingList)
 
 
 /*: question9
@@ -98,9 +112,27 @@
  */
 // write your code here
 
+func lifesEssential(ingredients: [String]) -> Bool
+{
+    if ingredients[0] == "Bread"
+    {
+    return true
+    }
+    else
+    {
+    return false
+    }
+}
 
 
-
+// Answwer 9
+func lifesEssential2(ingredients: Array<String>) -> Bool {
+    if ingredients[0] == "Bread" {
+        return true
+    } else {
+        return false
+    }
+}
 
 
 /*: question10
@@ -108,9 +140,10 @@
  */
 // write your code here
 
+print(lifesEssential(ingredients: shoppingList))
 
-
-
+// Answer 10
+print(lifesEssential(ingredients: shoppingList))
 
 
 
@@ -120,7 +153,11 @@
  */
 // write your code here
 
+print(lifesEssential(ingredients: desertList))
 
+// Answer 11
+
+print(lifesEssential(ingredients: desertList))
 
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-arrays-lab/blob/solution/Arrays.playground/Pages/solution.xcplaygroundpage/Contents.swift) for the solution.
