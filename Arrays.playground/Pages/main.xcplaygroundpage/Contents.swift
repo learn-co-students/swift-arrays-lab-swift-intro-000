931904e326c66df5033803ee0d3a46d77b964425
kentaloup <kentaloup@hotmail.com>
2017-07-27 21:34:00 -0700

    Finished Lab

--- Arrays.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ Arrays.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -13,6 +13,7 @@
  */
     // write your code here
 
+var list = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 
@@ -23,6 +24,7 @@
  */
 // write your code here
 
+var shoppingList: [String] = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 
@@ -32,6 +34,8 @@
  */
 // write your code here
 
+var futureShoppingList: [String]
+futureShoppingList = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 
@@ -42,6 +46,7 @@
  */
 // write your code here
 
+let cheeseSandwich: [String] = ["Bread", "Butter", "Cheese", "Lettuce", "Tomatoes"]
 
 
 
@@ -52,7 +57,7 @@
  */
 // write your code here
 
-
+var desertList: [String] = ["Cookie dough", "Icecream"]
 
 
 
@@ -62,7 +67,7 @@
  */
 // write your code here
 
-
+var afternoonAttendance: [Int] = [2, 10, 3, 15, 7]
 
 
 
@@ -74,7 +79,8 @@
  */
 // write your code here
 
-
+var itemToPrint = shoppingList[1]
+print(itemToPrint)
 
 
 
@@ -86,7 +92,8 @@
  */
 // write your code here
 
-
+futureShoppingList[2] = "Chicken"
+print(futureShoppingList)
 
 
 
@@ -98,7 +105,13 @@
  */
 // write your code here
 
-
+func lifesEssential(ingredients: [String]) -> Bool {
+    if ingredients[0] == "Bread" {
+        return true
+    } else {
+        return false
+    }
+}
 
 
 
@@ -108,7 +121,7 @@
  */
 // write your code here
 
-
+print(lifesEssential(ingredients: shoppingList))
 
 
 
@@ -120,7 +133,7 @@
  */
 // write your code here
 
-
+print(lifesEssential(ingredients: desertList))
 
 
 //: Click [here](https://github.com/learn-co-curriculum/swift-arrays-lab/blob/solution/Arrays.playground/Pages/solution.xcplaygroundpage/Contents.swift) for the solution.
